a14e31dab092cba4765cda9078935db02e1964c1
Michael Christopher <michaelchristophernewyork@hotmail.com>
2017-10-29 14:01:45 -0400

    add output

--- output.sh
+++ output.sh
@@ -8,3 +8,4 @@ Branch FEATURE-add-gitignore set up to track remote branch FEATURE-add-gitignore
 Branch master set up to track remote branch master from origin.
 Branch FEATURE-add-gitignore set up to track remote branch FEATURE-add-gitignore from origin.
 Branch master set up to track remote branch master from origin.
+Sun Oct 29 14:01:45 EDT 2017
