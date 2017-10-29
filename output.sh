9fb4d22675eaf41f9dc3b0626e8de1df4efd249d
Michael Christopher <michaelchristophernewyork@hotmail.com>
2017-10-29 13:58:12 -0400

    add output

--- output.sh
+++ output.sh
@@ -1,26 +1,4 @@
-tree d4c0b1d97a4c1293b1bee86bc58df6e79acd0c79
-parent 841364d2cfd80fff22e3d3205dbb69f3b31996a8
-author Michael Christopher <michaelchristophernewyork@hotmail.com> 1509297562 -0400
-committer Michael Christopher <michaelchristophernewyork@hotmail.com> 1509297562 -0400
-
-add output sh
-[?1049h[53;1H[?12;25h[?1h="/home/mcny/Documents/src/MyFirstProject/.git/COMMIT_EDITMSG" 10 lines, 262 characters [H[2J[45B# Please enter the commit message for your changes. Lines starting
-# with '#' will be ignored, and an empty message aborts the commit.
-#
-# On branch master
-# Your branch is up-to-date with 'origin/master'.
-#
-# Changes to be committed:[7A[8B"/home/mcny/Documents/src/MyFirstProject/.git/COMMIT_EDITMSG" 10 lines, 262 characters [8A[?5h[?5l
-[?5h[?5l
-[?5h[?5l
-[?5h[?5l
-[?5h[?5l[?1049l[?1049h[53;1H[?12;25h[?1h="/home/mcny/Documents/src/MyFirstProject/.git/COMMIT_EDITMSG" 10 lines, 262 characters [H[2J[45B# Please enter the commit message for your changes. Lines starting
-# with '#' will be ignored, and an empty message aborts the commit.
-#
-# On branch master
-# Your branch is up-to-date with 'origin/master'.
-#
-# Changes to be committed:[7A[8B"/home/mcny/Documents/src/MyFirstProject/.git/COMMIT_EDITMSG" 10 lines, 262 characters [8A[?5h[?5l[?1049lBranch FEATURE-add-gitignore set up to track remote branch FEATURE-add-gitignore from origin.
+Branch FEATURE-add-gitignore set up to track remote branch FEATURE-add-gitignore from origin.
 Branch master set up to track remote branch master from origin.
 Branch FEATURE-add-gitignore set up to track remote branch FEATURE-add-gitignore from origin.
 Branch master set up to track remote branch master from origin.
